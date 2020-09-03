fa46ec3ba2921b986317da977d43739be209758a
Kelleymb <kelleybuccieri@gmail.com>
2020-09-03 16:28:42 -0400

    connection issue fixed

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-    DATABASE_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/bottoms-up',
-    TEST_DATABASE_URL: process.env.TEST_DB_URL || 'postgresql://dunder_mifflin@localhost/bottoms-up-test',
+    DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/bottoms-up',
+    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder_mifflin@localhost/bottoms-up-test',
   }
