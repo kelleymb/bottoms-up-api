bfb36516cee22ecca1e50a721c86087217706e56
Kelleymb <kelleybuccieri@gmail.com>
2020-09-03 12:13:59 -0400

    dependencies installed

--- src/app.js
+++ src/app.js
@@ -8,14 +8,24 @@ const errorHandler = require('./middleware/error-handler')
 
 const app = express()
 
+const allowedOrigins = ['http://localhost:3000', 'https://bottoms-up-client.vercel.app/']
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+  origin: function(origin, callback) {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not' + 'allow access from the specified Origin'
+        return callback(new Error(msg), false)
+    }
+    return callback(null, true)
+}}))
 
+//testing endpoint
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
